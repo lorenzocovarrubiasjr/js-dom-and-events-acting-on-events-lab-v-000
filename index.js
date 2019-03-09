b357db8e2dd0f9dc4fea47ee829ad926643751c2
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-03-09 19:07:07 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  const doc = document.querySelector('form input');
+  return doc.value;
+};
+
+function addNewElementAsLi() {
+  const element = document.createElement("LI");
+  const value = document.createTextNode(retrieveEmployeeInformation());
+  element.append(value);
+  document.querySelector('.employee-list').appendChild(element);
+};
+
+function addNewLiOnClick() {
+  const submit = document.querySelector('input[type="submit"]');
+  submit.addEventListener('click', function(){
+    addNewElementAsLi();
+    const doc = document.querySelector('form input')
+    doc.value = '';
+  })
+};
+
+function clearEmployeeListOnLinkClick() {
+  const clearEmployeeList = document.querySelector("a[href='#']");
+  clearEmployeeList.addEventListener('click', function(){
+    const employeeList = document.querySelector('.employee-list');
+    while(employeeList.firstChild) {
+      employeeList.removeChild(employeeList.firstChild);
+    }
+  })
+};
